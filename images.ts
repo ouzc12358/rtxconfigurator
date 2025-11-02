b0a996efadd618948cf397bb2bd062741030ee5f
ouzc12358 <ouzc12358@hotmail.com>
2025-11-02 17:42:22 +0800

    feat: Migrate images to Google Cloud Storage
    Updates image URLs to point to a Google Cloud Storage bucket. This improves performance and reliability for image loading within the configurator.

--- images.ts
+++ images.ts
@@ -1,27 +1,27 @@
 
-export const defaultImage = 'https://i.postimg.cc/13Y7YV1C/3-4-1-rtx2300-2400g-base.png';
+export const defaultImage = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-1-rtx2300-2400g-base.png';
 
 // RTX2300A and RTX2400G Base Images
-export const rtx2300_2400g_le_5Mpa_base = 'https://i.postimg.cc/13Y7YV1C/3-4-1-rtx2300-2400g-base.png';
-export const rtx2300_2400g_gt_5Mpa_base = 'https://i.postimg.cc/13Y7YV1C/3-4-1-rtx2300-2400g-base.png'; // Using same base, connections differ
+export const rtx2300_2400g_le_5Mpa_base = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-1-rtx2300-2400g-base.png';
+export const rtx2300_2400g_gt_5Mpa_base = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-1-rtx2300-2400g-base-robust.png';
 
 // RTX2300A and RTX2400G Connection Images (<= 5 MPa)
-export const rtx2300_2400g_le_5Mpa_conn_1 = 'https://i.postimg.cc/J01fVv5W/3-4-2-le-5-Mpa-conn1-npt-female.png';
-export const rtx2300_2400g_le_5Mpa_conn_2 = 'https://i.postimg.cc/Y0z0fQYv/3-4-2-le-5-Mpa-conn2-npt-male.png';
-export const rtx2300_2400g_le_5Mpa_conn_3 = 'https://i.postimg.cc/k47g213j/3-4-2-le-5-Mpa-conn3-m20-male.png';
-export const rtx2300_2400g_le_5Mpa_conn_4 = 'https://i.postimg.cc/pXj2R1Bq/3-4-2-le-5-Mpa-conn4-g1-2-male.png';
+export const rtx2300_2400g_le_5Mpa_conn_1 = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-2-le-5Mpa-conn1-npt-female.png';
+export const rtx2300_2400g_le_5Mpa_conn_2 = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-2-le-5Mpa-conn2-npt-male.png';
+export const rtx2300_2400g_le_5Mpa_conn_3 = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-2-le-5Mpa-conn3-m20-male.png';
+export const rtx2300_2400g_le_5Mpa_conn_4 = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-2-le-5Mpa-conn4-g1-2-male.png';
 
 // RTX2300A and RTX2400G Connection Images (> 5 MPa)
-export const rtx2300_2400g_gt_5Mpa_conn_1 = 'https://i.postimg.cc/W3tTgrTf/3-4-3-gt-5-Mpa-conn1-npt-female.png';
-export const rtx2300_2400g_gt_5Mpa_conn_2 = 'https://i.postimg.cc/26Y4VpSt/3-4-3-gt-5-Mpa-conn2-npt-male.png';
-export const rtx2300_2400g_gt_5Mpa_conn_3 = 'https://i.postimg.cc/pr0RZDdD/3-4-3-gt-5-Mpa-conn3-m20-male.png';
-export const rtx2300_2400g_gt_5Mpa_conn_4 = 'https://i.postimg.cc/YSxXG2k9/3-4-3-gt-5-Mpa-conn4-g1-2-male.png';
+export const rtx2300_2400g_gt_5Mpa_conn_1 = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-2-gt-5Mpa-conn1-npt-female.png';
+export const rtx2300_2400g_gt_5Mpa_conn_2 = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-2-gt-5Mpa-conn2-npt-male.png';
+export const rtx2300_2400g_gt_5Mpa_conn_3 = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-2-gt-5Mpa-conn3-m20-male.png';
+export const rtx2300_2400g_gt_5Mpa_conn_4 = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-2-gt-5Mpa-conn4-g1-2-male.png';
 
 // RTX2400K and RTX2500D Base Image
-export const rtx2400k_2500d_base = 'https://i.postimg.cc/HkqYvTf8/3-4-4-rtx2400k-2500d-base.png';
+export const rtx2400k_2500d_base = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-3-rtx2400k-2500d-base.png';
 
 // Manifold Images
-export const manifold_ag_2_valve = 'https://i.postimg.cc/50dGgYfB/3-5-1-manifold-ag-2-valve.png';
-export const manifold_k_2_valve = 'https://i.postimg.cc/tCT93P89/3-5-2-manifold-k-2-valve.png';
-export const manifold_d_3_valve = 'https://i.postimg.cc/pLDtKjpP/3-5-3-manifold-d-3-valve.png';
-export const manifold_d_5_valve = 'https://i.postimg.cc/N0n8sY3R/3-5-4-manifold-d-5-valve.png';
+export const manifold_ag_2_valve = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-4-manifold-ag-2-valve.png';
+export const manifold_k_2_valve = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-5-manifold-k-2-valve.png';
+export const manifold_d_3_valve = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-6-manifold-d-3-valve.png';
+export const manifold_d_5_valve = 'https://storage.googleapis.com/aai-web-samples/product-configurator/3-4-7-manifold-d-5-valve.png';
